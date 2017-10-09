9a09b0497a1152754dddc9b147e8841739a89296
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-10-09 18:48:40 +0000

    first commit

---
+++ IntroToNode/objectives.md
@@ -0,0 +1,3 @@
+# Intro To Node
+# What is it?
+# Why are we learning it?
---
+++ expressDemo/demo.js
@@ -0,0 +1,14 @@
+// Library/framework = code written by someone else that can be used.
+// library   - when you call the library youre in control
+// -- library is a collection of functionality that you call! 
+
+// framework - control flow is inverted - framwork calls you! 
+// -- all the control flow is already in the framework!
+// takes all the basic stuff every app needs and packages it up! 
+// lets you focus on the content instead of all the basic stuff
+
+// eg express - web dev framework
+
+
+
+
