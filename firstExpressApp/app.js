ab6c2881edd6b8789d6c6f23abf19fa56475ac7a
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-10-11 17:03:05 +0000

    cloud9 commit

--- firstExpressApp/app.js
+++ firstExpressApp/app.js
@@ -12,6 +12,26 @@ app.get("/bye", function(req,res){
 app.get("/dog", function(req,res){
    res.send("MEOW!"); 
 });
+// order of routes super important! if catch all/* route is first
+// nothing will match it! 
+// only first matching route will run! 
+
+// route parameters/path variables/route variables! 
+// example .../r/:pageName
+app.get("/r/:subredditName", function(req,res){
+   var subreddit = req.params.subredditName;
+   res.send("Welcome to the " + subreddit.toUpperCase() + " subreddit!");
+});
+app.get("/a/:subredditName/comments/:id/:title", function(req,res){
+   var subreddit = req.params.subredditName;
+   res.send("Welcome to the " + subreddit.toUpperCase() + " subreddit!");
+   res.send("Welcome to another random page!");
+});
+
+app.get("*", function(req,res){
+   res.send("You are a STAR!");
+// useful for page not found etc.
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
