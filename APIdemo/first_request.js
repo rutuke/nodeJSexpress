d1f8bb7381b8ed0a99fbfbfec4923311dd09e169
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-10-14 11:26:01 +0000

    weather API

--- APIdemo/first_request.js
+++ APIdemo/first_request.js
@@ -1,6 +1,13 @@
 var request = require('request');
+
+console.log("Sunset in Hawaii for today is at...");
 request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(error, response,body){
-    console.log('error:', error); // prints error if error occured
-    console.log('statusCode:', response && response.statusCode );//prints repsponse status code
-    console.log('body:', body); // prints the html for home page.
+    if(!error && response.statusCode == 200){
+        var parsedData = JSON.parse(body);
+        console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]); // prints the html for home page.
+    } else {
+        console.log(error); // prints error if error occured
+        console.log(response && response.statusCode );//prints repsponse status code
+    }
+    
 });
