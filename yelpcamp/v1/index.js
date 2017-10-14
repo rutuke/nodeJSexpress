51e9bdde6f46dcc19b50f013a16c996ac6d6d447
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-10-14 14:36:16 +0000

    YelpCamp V1

--- yelpcamp/v1/index.js
+++ yelpcamp/v1/index.js
@@ -1,14 +1,11 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
-app.get("/", function(req,res){
-   res.render("landing"); 
-});
-
-app.get("/campgrounds", function(req,res){
-   var campgrounds = [
+var campgrounds = [
        {name: "Wicklow Mountains", image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Glendasan_River%2C_Wicklow_Mountains.jpg"},
        {name: "Dublin Mountains", image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Glendasan_River%2C_Wicklow_Mountains.jpg"},
        {name: "Wexford Mountains", image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Glendasan_River%2C_Wicklow_Mountains.jpg"},
@@ -17,9 +14,29 @@ app.get("/campgrounds", function(req,res){
        {name: "Sligo Mountains", image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Glendasan_River%2C_Wicklow_Mountains.jpg"},
        {name: "Kerry Mountains", image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Glendasan_River%2C_Wicklow_Mountains.jpg"},
        {name: "Meath Mountains", image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Glendasan_River%2C_Wicklow_Mountains.jpg"},
-       ]; 
+    ]; 
        
+app.get("/", function(req,res){
+   res.render("landing"); 
+});
+
+app.get("/campgrounds", function(req,res){
        res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req,res){
+    // get data from form
+    // add to campgrounds array
+    // redirect back to campgrounder page
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCamp = {name: name, image: image}
+    campgrounds.push(newCamp);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req,res){
+   res.render("new.ejs"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
