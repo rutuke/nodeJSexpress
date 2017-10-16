2309505c72689ffd876dd21aa735c4c654a90dbf
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-10-16 11:24:12 +0000

    RESTful Blog App Using Index

--- postAppy/app.js
+++ postAppy/app.js
@@ -1,19 +1,25 @@
 var express = require("express");
 var app = express();
 
+var bodyParser = require("body-parser");
+
  app.set("view engine", "ejs");
+ app.use(bodyParser.urlencoded({extended: true}));
+ 
+ var friends = ["Tony", "Miranda", "Justin", "Lily", "Hagrid"];
  
  app.get("/", function(req,res){
     res.render("home"); 
  });
  
  app.get("/friends", function(req,res){
-    var friends = ["Tony", "Miranda", "Justin", "Lily", "Hagrid"];
     res.render("friends", {friends: friends})
  });
  
  app.post("/addfriend", function(req,res){
-    res.send("You have reached the post route!") 
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends"); 
  });
  
  app.listen(process.env.PORT, process.env.IP, function(){
