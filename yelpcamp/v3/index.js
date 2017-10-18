5c1d0d9dcf8d3dcb8cf076bcabe8e4042c187155
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-10-18 12:12:20 +0000

    v3 of yelpcamp

---
+++ yelpcamp/v3/index.js
@@ -0,0 +1,79 @@
+var     express     = require("express"),
+        app         = express(),
+        bodyParser  = require("body-parser"),
+        mongoose    = require("mongoose"),
+        Campground  = require("./models/campground");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+
+// Campground.create({
+//     name: "Wicklow Mountains", 
+//     image: "http://innovationalsteps.org/uploads/3/4/7/8/34786860/5082015_orig.jpg",
+//     description: "This is a huge hill, no bathrooms. No water. Beautiful hill."
+// }, function(err,campground){
+//   if(err){
+//       console.log(err);
+//   } else {
+//       console.log("Newly Created Campground");
+//       console.log(campground);
+//   }
+// });
+
+app.get("/", function(req,res){
+   res.render("landing"); 
+});
+
+app.get("/campgrounds", function(req,res){
+    // get all campgrounds from db
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    });
+    //render them all
+    //res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req,res){
+    // get data from form
+    // add to campgrounds array
+    // redirect back to campgrounder page
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCamp = {name: name, image: image, description: desc}
+    // Create new campground and save to DB
+    Campground.create(newCamp, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }   else {
+             res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req,res){
+   res.render("new.ejs"); 
+});
+
+app.get("/campgrounds/:id", function(req,res){
+    // find campground with given ID
+    // render more information about that ID
+    Campground.findById(req.params.id, function(err,foundCampground){
+       if(err){
+           console.log(err);
+       } else {
+           res.render("show", {campground: foundCampground});
+       }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("YelpcCamp server has started! "); 
+});
