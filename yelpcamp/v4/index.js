e951cf4897c1ba60b524b76ad4216e4893767d01
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-10-27 14:14:06 +0000

    v4 yelp camp edit

--- yelpcamp/v4/index.js
+++ yelpcamp/v4/index.js
@@ -3,6 +3,7 @@ var     express     = require("express"),
         bodyParser  = require("body-parser"),
         mongoose    = require("mongoose"),
         Campground  = require("./models/campground"),
+        Comment     = require("./models/comment"),
         seedDB      = require("./seeds.js");
         
 mongoose.connect("mongodb://localhost/yelp_camp");
@@ -73,6 +74,29 @@ app.get("/campgrounds/:id/comments/new", function(req,res){
                res.render("comments/new", {campground: campground}); 
        }
     });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+   //lookup campground using ID
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+        Comment.create(req.body.comment, function(err, comment){
+           if(err){
+               console.log(err);
+           } else {
+               campground.comments.push(comment);
+               campground.save();
+               res.redirect('/campgrounds/' + campground._id);
+           }
+        });
+       }
+   });
+   //create new comment
+   //connect new comment to campground
+   //redirect campground show page
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
